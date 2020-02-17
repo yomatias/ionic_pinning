3333e70ff931529bef473b47f89f87cace939582
matías <mluna@goiar.com>
2020-02-17 18:46:28 -0300

    add cordova-plugin-advanced-http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,39 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+import { HTTP } from '@ionic-native/http';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private http: HTTP) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.http.setSSLCertMode('pinned')
+        .then(() => {
+          console.log('[SUCCESS] SSL Pinning Starts!’');
+          this.testURL('https://badssl.com/');
+          this.testURL('https://www.howsmyssl.com/a/check');
+        })
+        .catch((error) => console.log('[ERROR] SSL Pinning Fails!', error));
     });
   }
+
+  testURL(URL: string) {
+    this.http.get(URL, {}, {})
+      .then(data => {
+        console.log('[success] ' +URL);
+      })
+      .catch(error => {
+        console.log('[error] '+URL+' -> ', error);
+      });
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HTTP } from '@ionic-native/http';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -32,6 +34,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   providers: [
+    HTTP,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
